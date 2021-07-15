37a8c899f9ef030e6d976c905665773b477b557b
emilycentko <emily.centko@gmail.com>
2021-07-15 14:13:35 -0500

    first commit and initial setup

---
+++ setlistapi/models/__init__.py
@@ -0,0 +1,1 @@
+from .attendee import Attendee
---
+++ setlistapi/models/attendee.py
@@ -0,0 +1,10 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+
+class Attendee(models.Model):
+
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    bio = models.CharField(max_length=50)
+    city = models.CharField(max_length=50)
+    state = models.CharField(max_length=50)
